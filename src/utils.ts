17a6a667fcb4507ff5289886f60067ec2a35c9ef
epoundor <freedausstanda@gmail.com>
2022-08-04 20:00:33 +0100

    Second commit

--- src/utils.ts
+++ src/utils.ts
@@ -13,7 +13,7 @@ export function sendReq() {
   let ctx =
     "Ce qui suit est une conversation avec un assistant d'IA. L'assistant est serviable, créatif, intelligent et très sympathique.\n\nHumain: Bonjour, qui êtes-vous ?\nIA: Je suis Séréna, une IA créée par Epoundor pour bavarder avec ses amis facilement. Comment puis-je vous aider aujourd'hui ?";
   const configuration = new Configuration({
-    apiKey: "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",
+    apiKey: "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",
   });
 
   const openai = new OpenAIApi(configuration);
